c575bb1100bbb679c581db0e057c5b7c4582cc98
texiseduardo021-hub <texiseduardo021@gmail.com>
2026-03-24 23:01:19 -0600

    Update pelicula-vista-modifica.php

--- php/pelicula-vista-modifica.php
+++ php/pelicula-vista-modifica.php
@@ -1,1 +1,26 @@
+<?php
+require_once __DIR__ . "/lib/manejaErrores.php";
+require_once __DIR__ . "/lib/recibeEnteroObligatorio.php";
+require_once __DIR__ . "/lib/validaEntidadObligatoria.php";
+require_once __DIR__ . "/lib/devuelveJson.php";
+require_once __DIR__ . "/Bd.php";
 
+try {
+  $id = recibeEnteroObligatorio("id");
+  $bd = Bd::pdo();
+  $stmt = $bd->prepare("SELECT * FROM PELICULA WHERE PEL_ID = ?");
+  $stmt->execute([$id]);
+  $modelo = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  validaEntidadObligatoria($modelo);
+
+  // Mandamos los datos a los "value" del formulario
+  devuelveJson([
+    "id" => ["value" => $modelo["PEL_ID"]],
+    "titulo" => ["value" => $modelo["PEL_TITULO"]],
+    "genero" => ["value" => $modelo["PEL_GENERO"]],
+    "sinopsis" => ["value" => $modelo["PEL_SINOPSIS"]]
+  ]);
+} catch (Throwable $e) {
+  manejaErrores($e);
+}
