d2afca4cb31aaf891da88b85b75775ee5d15aeb3
texiseduardo021-hub <texiseduardo021@gmail.com>
2026-03-24 23:23:28 -0600

    Update enviaFormRecibeJson.js

--- js/lib/enviaFormRecibeJson.js
+++ js/lib/enviaFormRecibeJson.js
@@ -1,1 +1,11 @@
-
+export async function enviaFormRecibeJson(event, url, destino) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const response = await fetch(url, { method: "POST", body: formData });
+  if (response.ok) {
+    location.href = destino;
+  } else {
+    const error = await response.json();
+    alert(error.detalle || "Error al procesar");
+  }
+}
