ebb1c0c610a39aec46ad775fa1f565d4d548e489
texiseduardo021-hub <texiseduardo021@gmail.com>
2026-03-24 23:30:54 -0600

    Update descargaVista.js

--- js/lib/descargaVista.js
+++ js/lib/descargaVista.js
@@ -1,15 +1,11 @@
 export async function descargaVista(url, id) {
   const elemento = document.getElementById(id);
   if (elemento) {
-    try {
-      const response = await fetch(url);
-      if (response.ok) {
-        elemento.innerHTML = await response.text();
-      } else {
-        elemento.innerHTML = "Error al cargar la vista";
-      }
-    } catch (e) {
-      elemento.innerHTML = "Error de red";
+    const response = await fetch(url);
+    if (response.ok) {
+      elemento.innerHTML = await response.text();
+    } else {
+      elemento.innerHTML = "Error al descargar la vista.";
     }
   }
 }
