f58b69968a71b38c565e575b386b08680123c54a
texiseduardo021-hub <texiseduardo021@gmail.com>
2026-03-24 22:50:55 -0600

    Update pelicula-elimina.php

--- php/pelicula-elimina.php
+++ php/pelicula-elimina.php
@@ -1,1 +1,16 @@
+<?php
+require_once __DIR__ . "/lib/manejaErrores.php";
+require_once __DIR__ . "/lib/recibeTextoObligatorio.php";
+require_once __DIR__ . "/lib/devuelveJson.php";
+require_once __DIR__ . "/Bd.php";
 
+try {
+  $id = recibeTextoObligatorio("id");
+  $bd = Bd::pdo();
+  $stmt = $bd->prepare("DELETE FROM PELICULA WHERE PEL_ID = ?");
+  $stmt->execute([$id]);
+
+  devuelveJson(["mensaje" => "Película eliminada"]);
+} catch (Throwable $e) {
+  manejaErrores($e);
+}
