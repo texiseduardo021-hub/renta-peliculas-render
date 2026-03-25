99eb5eefb51cdba9b1615cf02ef674d3080da6f2
texiseduardo021-hub <texiseduardo021@gmail.com>
2026-03-24 22:50:01 -0600

    Update recibeTextoObligatorio.php

--- php/lib/recibeTextoObligatorio.php
+++ php/lib/recibeTextoObligatorio.php
@@ -1,1 +1,7 @@
-
+<?php
+function recibeTextoObligatorio($nombre) {
+  if (isset($_POST[$nombre]) && trim($_POST[$nombre]) !== "") {
+    return $_POST[$nombre];
+  }
+  throw new Exception("Falta el campo obligatorio: $nombre");
+}
