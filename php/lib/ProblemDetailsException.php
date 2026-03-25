9270fc166d53fd74d0a53879921b68538750c355
texiseduardo021-hub <texiseduardo021@gmail.com>
2026-03-24 23:09:14 -0600

    Update ProblemDetailsException.php

--- php/lib/ProblemDetailsException.php
+++ php/lib/ProblemDetailsException.php
@@ -1,1 +1,11 @@
-
+<?php
+class ProblemDetailsException extends Exception {
+  public function __construct(
+    public int $status,
+    public string $title,
+    public string $type,
+    public string $detail
+  ) {
+    parent::__construct($detail);
+  }
+}
