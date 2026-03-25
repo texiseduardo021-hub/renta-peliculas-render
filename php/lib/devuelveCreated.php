78062b56115acdd8cbbabd170d87e9c1aa0965fd
texiseduardo021-hub <texiseduardo021@gmail.com>
2026-03-24 22:58:54 -0600

    Update devuelveCreated.php

--- php/lib/devuelveCreated.php
+++ php/lib/devuelveCreated.php
@@ -1,1 +1,7 @@
-
+<?php
+function devuelveCreated($uri, $datos) {
+  header("Location: " . $uri);
+  http_response_code(201);
+  header("Content-Type: application/json");
+  echo json_encode($datos);
+}
