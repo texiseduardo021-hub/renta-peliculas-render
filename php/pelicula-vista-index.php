fe17c1b00f08050a1098f665bf052389a279022c
texiseduardo021-hub <texiseduardo021@gmail.com>
2026-03-24 23:20:02 -0600

    Update pelicula-vista-index.php

--- php/pelicula-vista-index.php
+++ php/pelicula-vista-index.php
@@ -1,11 +1,11 @@
 <?php
+// php/pelicula-vista-index.php
 require_once __DIR__ . "/lib/manejaErrores.php";
 require_once __DIR__ . "/lib/devuelveJson.php";
 require_once __DIR__ . "/Bd.php";
 
 try {
   $bd = Bd::pdo();
-  // Seleccionamos tus campos de la tabla PELICULA
   $stmt = $bd->query("SELECT * FROM PELICULA ORDER BY PEL_TITULO");
   $modelos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -13,16 +13,11 @@ try {
   foreach ($modelos as $modelo) {
     $id = urlencode($modelo["PEL_ID"]);
     $titulo = htmlentities($modelo["PEL_TITULO"]);
-    $genero = htmlentities($modelo["PEL_GENERO"]);
-    
-    // Estructura de lista igual a la del profesor
     $render .= "<li>
-      <p>
-        <a href='modifica.html?id=$id'>$titulo</a>
-        <br>
-        <small>$genero</small>
-      </p>
-    </li>";
+                  <p>
+                    <a href='modifica.html?id=$id'>$titulo</a>
+                  </p>
+                </li>";
   }
 
   devuelveJson(["lista" => ["innerHTML" => $render]]);
