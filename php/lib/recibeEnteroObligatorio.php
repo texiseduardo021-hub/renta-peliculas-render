e4b66876a0362c6e3e31e9bc1c2240022f29291f
texiseduardo021-hub <texiseduardo021@gmail.com>
2026-03-24 22:52:59 -0600

    Update recibeEnteroObligatorio.php

--- php/lib/recibeEnteroObligatorio.php
+++ php/lib/recibeEnteroObligatorio.php
@@ -1,1 +1,7 @@
-
+<?php
+function recibeEnteroObligatorio($nombre) {
+  if (isset($_POST[$nombre]) && filter_var($_POST[$nombre], FILTER_VALIDATE_INT) !== false) {
+    return (int)$_POST[$nombre];
+  }
+  throw new Exception("Falta el id o no es válido: $nombre");
+}
