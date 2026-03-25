489301c93b8b6d89fc470e41c9e2790b0bed0e75
texiseduardo021-hub <texiseduardo021@gmail.com>
2026-03-24 23:01:39 -0600

    Update pelicula-modifica.php

--- php/pelicula-modifica.php
+++ php/pelicula-modifica.php
@@ -1,1 +1,28 @@
+<?php
+require_once __DIR__ . "/lib/manejaErrores.php";
+require_once __DIR__ . "/lib/recibeEnteroObligatorio.php";
+require_once __DIR__ . "/lib/recibeTextoObligatorio.php";
+require_once __DIR__ . "/lib/devuelveJson.php";
+require_once __DIR__ . "/Bd.php";
 
+try {
+  $id = recibeEnteroObligatorio("id");
+  $titulo = recibeTextoObligatorio("titulo");
+  $genero = recibeTextoObligatorio("genero");
+  $sinopsis = recibeTextoObligatorio("sinopsis");
+
+  $bd = Bd::pdo();
+  $stmt = $bd->prepare(
+    "UPDATE PELICULA SET PEL_TITULO = ?, PEL_GENERO = ?, PEL_SINOPSIS = ? WHERE PEL_ID = ?"
+  );
+  $stmt->execute([$titulo, $genero, $sinopsis, $id]);
+
+  devuelveJson([
+    "id" => $id,
+    "titulo" => $titulo,
+    "genero" => $genero,
+    "sinopsis" => $sinopsis
+  ]);
+} catch (Throwable $e) {
+  manejaErrores($e);
+}
