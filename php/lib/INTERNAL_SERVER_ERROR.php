a7e956d2fe1d22474c7e5f6b7eb567b4a57bfa51
texiseduardo021-hub <texiseduardo021@gmail.com>
2026-03-24 23:10:40 -0600

    Update INTERNAL_SERVER_ERROR.php

--- php/lib/INTERNAL_SERVER_ERROR.php
+++ php/lib/INTERNAL_SERVER_ERROR.php
@@ -1,6 +1,10 @@
 <?php
-function INTERNAL_SERVER_ERROR(Throwable $e) {
+function ERROR_DEL_SERVIDOR_INTERNO(Throwable $e) {
   http_response_code(500);
-  echo json_encode(["detalle" => $e->getMessage()]);
+  echo json_encode([
+    "title" => "Error interno del servidor",
+    "status" => 500,
+    "detail" => $e->getMessage()
+  ]);
   exit;
 }
